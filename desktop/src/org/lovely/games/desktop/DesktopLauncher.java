4bbf599ef28ac40a42bf9d4b06c383e3e2f87746
Sean O'Neill <seansaysmeh@gmail.com>
2018-04-01 18:15:01 +0100

    initial commit

---
+++ desktop/src/org/lovely/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.lovely.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.lovely.games.BastilleMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BastilleMain(), config);
+	}
+}
